08f63deab8d167669a3aca9e0cee0fe733dc2c5a
Bogdan Kiptenko <bogdankiptenko@gmail.com>
2020-03-30 13:45:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Square s1 = new Square(5);
+            Square s2 = new Square(2);
+            Square s3 = new Square(5);
+
+            Console.WriteLine("\tSquare perimeters");
+            Console.WriteLine("s1 = {0}", s1.GetPerimeter);
+            Console.WriteLine("s2 = {0}", s2.GetPerimeter);
+            Console.WriteLine("s3 = {0}\n", s3.GetPerimeter);
+
+            if (s1.CompareTo(s2) == 1)
+            {
+                Console.WriteLine("s1 > s2");
+            }
+            else if(s1.CompareTo(s2) == 0)
+            {
+                Console.WriteLine("s1 = s2");
+            }
+            else
+            {
+                Console.WriteLine("s1 < s2");
+            }
+
+            if (s1.CompareTo(s3) == 1)
+            {
+                Console.WriteLine("s1 > s3");
+            }
+            else if (s1.CompareTo(s3) == 0)
+            {
+                Console.WriteLine("s1 = s3");
+            }
+            else
+            {
+                Console.WriteLine("s1 < s3");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6Task1
+{
+    public class Square: IComparable<Square>
+    {
+        double side;
+        double perimeter;
+        public Square(double s)
+        {
+            this.side = s;
+            this.perimeter = s * 4;
+        }
+        public double GetPerimeter
+        {
+            get
+            {
+                return perimeter;
+            }
+        }
+        public int CompareTo(Square s2)
+        {
+            return this.perimeter.CompareTo(s2.perimeter);
+        }
+    }
+}
